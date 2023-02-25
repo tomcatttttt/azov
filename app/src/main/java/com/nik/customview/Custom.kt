4efdadc4a38a35b0887ba9d9c7dfb52d8bc0a2b4
Nikita Karetnikov <myipodtouch2014@gmail.com>
2023-02-25 23:22:08 +0200

    done

--- app/src/main/java/com/nik/customview/Custom.kt
+++ app/src/main/java/com/nik/customview/Custom.kt
@@ -3,12 +3,9 @@ package com.nik.customview
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
-import kotlin.math.sin
-import android.view.View
-import kotlin.math.cos
+import android.graphics.Path
+
 import androidx.appcompat.widget.AppCompatButton
-import androidx.core.content.res.ResourcesCompat
 
 class StarView @JvmOverloads constructor(
     context: Context,
@@ -22,7 +19,9 @@ class StarView @JvmOverloads constructor(
     private var secondStarColor = black
     private var thirdStarColor = black
     private val paint = Paint(Paint.FILTER_BITMAP_FLAG)
-    private val path = Path()
+     val path = Path()
+    private val bounds = RectF()
+
 
     init {
         setOnClickListener {
@@ -71,7 +70,15 @@ class StarView @JvmOverloads constructor(
         }
     }
 
+    fun getBounds(): RectF {
+        path.computeBounds(bounds, true)
+        return bounds
+    }
+
+
     private fun drawFirstAzov(color: Int, canvas: Canvas) {
+        val scaleFactor = width / 600f
+
         paint.shader = LinearGradient(
             0f, 0f, 0f,
             height.toFloat(),
@@ -80,23 +87,24 @@ class StarView @JvmOverloads constructor(
             Shader.TileMode.MIRROR
         )
 
-        path.moveTo(width.toFloat() / 7.4f, 0f)
-        path.lineTo(width.toFloat() / 7.4f, 367f)
-        path.lineTo(width.toFloat() / 5.6f, 367f)
-        path.lineTo(width.toFloat() / 5.6f, 291f)
-        path.lineTo(width.toFloat() / 4.45f, 306f)
-        path.lineTo(width.toFloat() / 4.45f, 439f)
-        path.lineTo(width.toFloat() / 3.45f, 471f)
-        path.lineTo(width.toFloat() / 3.45f, 327f)
-        path.lineTo(width.toFloat() / 2.49f, 367f)
-        path.lineTo(width.toFloat() / 2.49f, 223f)
-        path.lineTo(width.toFloat() / 2.8117f, 223f)
-        path.lineTo(width.toFloat() / 2.8117f, 280f)
-        path.lineTo(width.toFloat() / 3.4f, 257f)
-        path.lineTo(width.toFloat() / 3.4f, 133f)
-        path.lineTo(width.toFloat() / 4.45f, 117f)
-        path.lineTo(width.toFloat() / 4.45f, 233f)
-        path.lineTo(width.toFloat() / 7.4f, 200f)
+        path.reset()
+        path.moveTo(75f * scaleFactor, 0f)
+        path.lineTo(75f * scaleFactor, 367f * scaleFactor)
+        path.lineTo(96f * scaleFactor, 367f * scaleFactor)
+        path.lineTo(96f * scaleFactor, 291f * scaleFactor)
+        path.lineTo(124f * scaleFactor, 306f * scaleFactor)
+        path.lineTo(124f * scaleFactor, 439f * scaleFactor)
+        path.lineTo(170f * scaleFactor, 471f * scaleFactor)
+        path.lineTo(170f * scaleFactor, 327f * scaleFactor)
+        path.lineTo(236f * scaleFactor, 367f * scaleFactor)
+        path.lineTo(236f * scaleFactor, 223f * scaleFactor)
+        path.lineTo(209f * scaleFactor, 223f * scaleFactor)
+        path.lineTo(209f * scaleFactor, 280f * scaleFactor)
+        path.lineTo(175f * scaleFactor, 257f * scaleFactor)
+        path.lineTo(175f * scaleFactor, 133f * scaleFactor)
+        path.lineTo(124f * scaleFactor, 117f * scaleFactor)
+        path.lineTo(124f * scaleFactor, 233f * scaleFactor)
+        path.lineTo(75f * scaleFactor, 200f * scaleFactor)
 
         path.close()
         canvas.drawPath(path, paint)
--- app/src/main/java/com/nik/customview/MainActivity.kt
+++ app/src/main/java/com/nik/customview/MainActivity.kt
@@ -4,10 +4,30 @@ import android.content.Context
 import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nik.customview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            applyButton.setOnClickListener {
+                val width = widthEditText.text.toString().toIntOrNull() ?: starView.width
+                val height = heightEditText.text.toString().toIntOrNull() ?: starView.height
+                val starWidth = starView.width.toFloat() / 3.6234f
+                val starHeight =
+                    starWidth / starView.getBounds().width() * starView.getBounds()
+                        .height()
+                val scaleWidth = width / starWidth
+                val scaleHeight = height / starHeight
+                starView.layoutParams = starView.layoutParams.apply {
+                    this.width = width
+                    this.height = (scaleWidth.coerceAtMost(scaleHeight) * starHeight).toInt()
+                }
+            }
+        }
     }
 }
